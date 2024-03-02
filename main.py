3d016d64640bf2afe388baab5ee10b81fa05b8c2
f0dor <markopcstuff@gmail.com>
2024-03-02 07:35:52 +0100

    made code device agnostic

--- main.py
+++ main.py
@@ -6,14 +6,9 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 import time
 
-if torch.cuda.is_available():
-    print("CUDA is available! We can use GPU acceleration.")
-
-    device = torch.device("cuda")
-    print("Using:", torch.cuda.get_device_name(0))
-else:
-    print("CUDA is not available. Using CPU instead.")
-    device = torch.device("cpu")
+device = "cuda" if torch.cuda.is_available() else "cpu"
+device = "cpu"
+print(device)
 
 bc = datasets.load_breast_cancer()
 X, y = bc.data, bc.target
@@ -25,10 +20,11 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-X_train = torch.from_numpy(X_train.astype(numpy.float32))
-X_test = torch.from_numpy(X_test.astype(numpy.float32))
-y_train = torch.from_numpy(y_train.astype(numpy.float32))
-y_test = torch.from_numpy(y_test.astype(numpy.float32))
+X_train = torch.from_numpy(X_train.astype(numpy.float32)).to(device)
+X_test = torch.from_numpy(X_test.astype(numpy.float32)).to(device)
+y_train = torch.from_numpy(y_train.astype(numpy.float32)).to(device)
+y_test = torch.from_numpy(y_test.astype(numpy.float32)).to(device)
+
 
 y_train = y_train.view(y_train.shape[0], 1)
 y_test = y_test.view(y_test.shape[0], 1)
@@ -42,16 +38,19 @@ class LogisticRegression(nn.Module):
         y_predicted = torch.sigmoid(self.linear(x))
         return y_predicted
 
-model = LogisticRegression(n_features)
-learning_rate = 0.01
+model = LogisticRegression(n_features).to(device)
+learning_rate = 0.001
 criterion = nn.BCELoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
 start_time = time.time()
 
-num_epochs = 1000
+num_epochs = 10000
 for epoch in range(num_epochs):
-    y_predicted = model(X_train)
+    for params in model.parameters():
+        params.to(device)
+    model.train()
+    y_predicted = model(X_train.to(device))
     loss = criterion(y_predicted, y_train)
     loss.backward()
     optimizer.step()
@@ -65,7 +64,7 @@ elapsed_time = end_time - start_time
 print(f'elapsed time: {elapsed_time:.4f} seconds')
 
 with torch.no_grad():
-    y_predicted = model(X_test)
+    y_predicted = model(X_test.to(device))
     y_predicted_cls = y_predicted.round()
     acc = y_predicted_cls.eq(y_test).sum() / float(y_test.shape[0])
     print(f'accuracy = {acc:.10f}')
