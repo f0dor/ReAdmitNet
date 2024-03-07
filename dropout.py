5b333b9973273f7a353bde972cde77ae8d8a576f
f0dor <markopcstuff@gmail.com>
2024-03-07 14:45:19 +0100

    push it baby

--- dropout.py
+++ dropout.py
@@ -2,59 +2,59 @@ import torch
 import torch.nn as nn
 import sklearn
 import pandas as pd
-from sklearn.model_selection import train_test_split
-import time
+from sklearn.metrics import matthews_corrcoef
 from sklearn.model_selection import KFold
+import time
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-data = pd.read_csv('./Tim_22/Podaci/train_modified_encoded1.csv')
-set_to_evaluate = pd.read_csv('./Tim_22/Podaci/test_modified_encoded1.csv')
+data = pd.read_csv('./Tim_22/Podaci/train_modified_encoded2.csv')
+set_to_evaluate = pd.read_csv('./Tim_22/Podaci/test_modified_encoded2.csv')
 
 X_eval = torch.tensor(set_to_evaluate.drop('Label', axis=1).values, dtype=torch.float32)
 X_eval = X_eval.to(device)
 
-
-# Separate features and target variable
 X = data.drop(columns=['Label'])
 y = data['Label']
 
-# Define your model
+# Compute class weights
+num_negatives = (y == 0).sum().item()
+num_positives = (y == 1).sum().item()
+total_samples = len(y)
+class_weights = torch.tensor([total_samples / (2 * num_negatives), total_samples / (2 * num_positives)]).to(device)
+print(f"Class weights: {class_weights}")
 class Model(nn.Module):
     def __init__(self):
         super(Model, self).__init__()
-        self.fc1 = nn.Linear(in_features=323, out_features=512)
-        self.fc2 = nn.Linear(in_features=512, out_features=128)
+        self.fc1 = nn.Linear(in_features=323, out_features=256)
+        self.fc2 = nn.Linear(in_features=256, out_features=128)
         self.fc3 = nn.Linear(in_features=128, out_features=1)
         self.relu = nn.ReLU()
-        self.dropout = nn.Dropout(p=0.5)  # Define dropout layer with dropout probability
+        self.dropout = nn.Dropout(p=0.5)
 
     def forward(self, x):
         x = self.relu(self.fc1(x))
-        x = self.dropout(x)  # Apply dropout after the activation function
+        x = self.dropout(x)
         x = self.relu(self.fc2(x))
-        x = self.dropout(x)  # Apply dropout after the activation function
+        x = self.dropout(x)
         x = self.fc3(x)
         return x
 
-# Initialize your model and move it to the appropriate device
 model = Model().to(device)
 
-# Define your loss function and optimizer
 loss_fn = nn.BCEWithLogitsLoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
 
-# Convert your data to tensors and move them to the appropriate device
 X = torch.tensor(X.values, dtype=torch.float32).to(device)
 y = torch.tensor(y.values, dtype=torch.float32).to(device)
 
-# Define your training and testing loops
-epochs = 5000
+epochs = 50
 kf = KFold(n_splits=5, shuffle=True, random_state=42)
 start_time = time.time()
 
+'''
 for epoch in range(epochs):
-    model.train()  # Set the model to training mode
+    model.train()
     train_losses = []
     train_accuracies = []
     test_losses = []
@@ -63,6 +63,8 @@ for epoch in range(epochs):
     for train_index, test_index in kf.split(X):
         X_train_fold, X_test_fold = X[train_index], X[test_index]
         y_train_fold, y_test_fold = y[train_index], y[test_index]
+
+        weights = class_weights[y_train_fold.long()]
         
         y_logits = model(X_train_fold).squeeze()
         y_pred = torch.round(torch.sigmoid(y_logits))
@@ -78,13 +80,14 @@ for epoch in range(epochs):
         train_accuracies.append(acc)
         
         with torch.no_grad():
-            model.eval()  # Set the model to evaluation mode
+            model.eval()
             y_logits_test = model(X_test_fold).squeeze()
             y_pred_test = torch.round(torch.sigmoid(y_logits_test))
             correct_test = torch.eq(y_pred_test, y_test_fold).sum().item()
             total_test = len(y_test_fold)
             acc_test = (correct_test / total_test) * 100
             loss_test = loss_fn(y_logits_test, y_test_fold)
+            mcc_val = matthews_corrcoef(y_test_fold.to('cpu').numpy(), y_pred_test.to('cpu').numpy())
             
             test_losses.append(loss_test.item())
             test_accuracies.append(acc_test)
@@ -94,20 +97,71 @@ for epoch in range(epochs):
     avg_test_loss = sum(test_losses) / len(test_losses)
     avg_test_acc = sum(test_accuracies) / len(test_accuracies)
     
-    print(f"Epoch [{epoch+1}/{epochs}], Train Loss: {avg_train_loss:.4f}, Train Accuracy: {avg_train_acc:.2f}%, Test Loss: {avg_test_loss:.4f}, Test Accuracy: {avg_test_acc:.2f}%")
+    print(f"Epoch [{epoch+1}/{epochs}], Train Loss: {avg_train_loss:.4f}, Train Accuracy: {avg_train_acc:.2f}%, Test Loss: {avg_test_loss:.4f}, Test Accuracy: {avg_test_acc:.2f}%, MCC: {mcc_val:.4f}")
+'''
+batch_size = 256
+
+for epoch in range(epochs):
+    model.train()
+    train_losses = []
+    train_accuracies = []
+    test_losses = []
+    test_accuracies = []
+    
+    for train_index, test_index in kf.split(X):
+        X_train_fold, X_test_fold = X[train_index], X[test_index]
+        y_train_fold, y_test_fold = y[train_index], y[test_index]
+
+        num_batches = len(X_train_fold) // batch_size
+        for batch_idx in range(num_batches):
+            start_idx = batch_idx * batch_size
+            end_idx = min((batch_idx + 1) * batch_size, len(X_train_fold))
+
+            X_batch = X_train_fold[start_idx:end_idx]
+            y_batch = y_train_fold[start_idx:end_idx]
+            
+            y_logits = model(X_batch).squeeze()
+            y_pred = torch.round(torch.sigmoid(y_logits))
+            loss = loss_fn(y_logits, y_batch)
+            correct_train = torch.eq(y_pred, y_batch).sum().item()
+            total_train = len(y_batch)
+            acc = (correct_train / total_train) * 100
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            
+            train_losses.append(loss.item())
+            train_accuracies.append(acc)
+        
+        with torch.no_grad():
+            model.eval()
+            y_logits_test = model(X_test_fold).squeeze()
+            y_pred_test = torch.round(torch.sigmoid(y_logits_test))
+            correct_test = torch.eq(y_pred_test, y_test_fold).sum().item()
+            total_test = len(y_test_fold)
+            acc_test = (correct_test / total_test) * 100
+            loss_test = loss_fn(y_logits_test, y_test_fold)
+            mcc_val = matthews_corrcoef(y_test_fold.to('cpu').numpy(), y_pred_test.to('cpu').numpy())
+            
+            test_losses.append(loss_test.item())
+            test_accuracies.append(acc_test)
+    
+    avg_train_loss = sum(train_losses) / len(train_losses)
+    avg_train_acc = sum(train_accuracies) / len(train_accuracies)
+    avg_test_loss = sum(test_losses) / len(test_losses)
+    avg_test_acc = sum(test_accuracies) / len(test_accuracies)
+    
+    print(f"Epoch [{epoch+1}/{epochs}], Train Loss: {avg_train_loss:.4f}, Train Accuracy: {avg_train_acc:.2f}%, Test Loss: {avg_test_loss:.4f}, Test Accuracy: {avg_test_acc:.2f}%, MCC: {mcc_val:.4f}")
 
 end_time = time.time()
 elapsed_time = end_time - start_time
 print(f"Training time: {elapsed_time:.2f}s")
 
-# Save your model if needed
 # torch.save(model.state_dict(), 'trained_model.pth')
 
-# Load the evaluation set
 X_eval = torch.tensor(set_to_evaluate.drop('Label', axis=1).values, dtype=torch.float32).to(device)
 
-# Perform inference on the evaluation set
-model.eval()  # Set the model to evaluation mode
+model.eval()
 
 with torch.no_grad():
     y_logits_eval = model(X_eval).squeeze()
